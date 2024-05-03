066792e501c5a0689a3af5809179d1b9ae165aca
Jennings Crane <jenningc@pr-n5.kzn.21-school.ru>
2024-05-03 12:01:09 +0300

    start project

---
+++ src/test/java/ru/jenningc/chamomile/ChamomileApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jenningc.chamomile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamomileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
